14363f998a33875af3c89bb9859e763eedd8fbbb
LamaVi2000 <dthjif2010@mail.ru>
2022-12-22 00:21:02 +1000

    Последовательность

--- Homeworks/DZ__2/Example_3.py
+++ Homeworks/DZ__2/Example_3.py
@@ -5,9 +5,7 @@
 # - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 n=int(input('Введите число:'))
-my_dict={}
-for i in range(1, n+1):
-    my_dict[i]= (1+(1/n))^n
-print(my_dict)
+lst = [round((1+1/i)**i) for i in range(1,n+1)]
+print(f'Полученная сумма последовательности {lst} =', sum(lst))
 
 
