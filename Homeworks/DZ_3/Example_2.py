e1fd9d8aae5664d7a8239aa507085a7716f64e35
LamaVi2000 <dthjif2010@mail.ru>
2022-12-30 14:13:32 +1000

    Произведение соседних чисел

--- Homeworks/DZ_3/Example_2.py
+++ Homeworks/DZ_3/Example_2.py
@@ -8,11 +8,11 @@
 # - [2, 3, 5, 6] => [12, 15]
 
 
-def mult_lst(lst):
+def mult(lst):
     l = len(lst)//2 + 1  if len(lst) % 2 != 0 else len(lst)//2  # //-целочисленное деление
     new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
     print(new_lst)
 
 lst = []
 lst = list(map(int, input("Введите числа через пробел:\n").split()))
-mult_lst(lst)
+mult(lst)
